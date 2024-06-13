53c711e3ab0dc6012499fe0998a8804f885798c6
VenkataSairaja <137163264+VenkataSairaja@users.noreply.github.com>
2024-06-13 15:57:19 +0530

    Add files via upload

---
+++ CRUD Application Using Spring Boot and Spring Data JPA Scenario/EmployeeService.java
@@ -0,0 +1,34 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(int id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
